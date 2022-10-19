99bd327fe2d8ac2478cfe6326ce721a02659362e
github ParkBig <github enfantgu@gmail.com>
2022-10-19 19:02:45 +0900

    tt

--- src/pages/TodoDetail.js
+++ src/pages/TodoDetail.js
@@ -1,8 +1,25 @@
-// 파이팅!
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { getToDo } from "../store/modules/toDoList";
+
+
 
 const TodoDetail = () => {
+    const dispatch = useDispatch();
+    useEffect(()=> {
+        const getTodo = async () => {
+            dispatch(getToDo(["TO_DO", (await axios.get("http://localhost:3001/TO_DO")).data]))
+        };
+        getTodo();
+    }, []);
+    const toDoData = (useSelector(state=>state.toDoList.value))["TO_DO"];
     return (
-        <></>
+        <div>
+            hi
+        </div>
     )
 }
 export default TodoDetail;
